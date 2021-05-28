a78ffcbcce5bdafe4e9f5264407d233f5e7206c0
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-05-28 14:21:51 +0200

    ✅ : Adding about page tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,4 @@
-from pages.views import HomePageView
+from pages.views import HomePageView, AboutPageView
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
@@ -27,4 +27,31 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTests(SimpleTestCase):
+
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_templates(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_contains_html(self):
+        self.assertContains(self.response, "About Page")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there! I should not be here in this page")
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
         )
